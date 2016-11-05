86c30836da209041e65a593c6136c873cbaf2e53
Jessica Temporal <jessicatemporal@gmail.com>
2016-11-05 18:24:57 -0200

    initial commit: adds __init__ and setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='caipyra',
+      version='0.1',
+      description='Caipyra 2016 poem',
+      url='http://github.com/jtemporal/caipyra',
+      author='Jessica Temporal',
+      author_email='jessicatemporal@gmail.com',
+      license='MIT',
+      packages=['caipyra'],
+      zip_safe=False)
