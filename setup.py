4ecd05987805a02c36ea9918add196f3542cbb39
Leticia <lcbm2@cin.ufpe.br>
2018-10-06 15:57:39 -0300

    Nova versão atualizada

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='caipyra',
-      version='0.3',
+      version='0.4',
       description='Caipyra 2016 poem',
       url='http://github.com/jtemporal/caipyra',
       author='Jessica Temporal',
